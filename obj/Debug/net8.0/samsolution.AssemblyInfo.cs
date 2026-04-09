f97b54c548dbc1ea78557eec06fbccfaccf83a1e
Samuel <samuel201927@outlook.com>
2026-04-08 23:05:09 -0300

    CARROSSEL

--- obj/Debug/net8.0/samsolution.AssemblyInfo.cs
+++ obj/Debug/net8.0/samsolution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("samsolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97a70f4f51f5031a5ce9e97c6f8fe55f2ddc54e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6306fccf9c3c123af3123837f2d3dfff9c593b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("samsolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("samsolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
